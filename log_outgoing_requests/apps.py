cbb64db0591c1e2ec22155d4a9dd7bcd1a83f5a9
Sergei Maertens <sergei@maykinmedia.nl>
2023-06-08 15:54:19 +0200

    :card_file_box: [#6] -- update migrations
    * Updated migration for changed Meta options
    * Change default ID field to BigAutoField
    
    The default_auto_field attribute is supported from Django 3.2+ onwards,
    so we can divert from the legacy default and use proper 64 bit fields.
    
    Log records in particular can have very high volumes, which makes this
    future proof.

--- log_outgoing_requests/apps.py
+++ log_outgoing_requests/apps.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class LogOutgoingRequestsConfig(AppConfig):
     name = "log_outgoing_requests"
     verbose_name = _("Outgoing request logs")
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         from .log_requests import install_outgoing_requests_logging
