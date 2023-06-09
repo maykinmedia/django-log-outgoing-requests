5d665ef63bb5db64425c31c4c6af677b9ccdd73a
Sergei Maertens <sergei@maykinmedia.nl>
2023-06-09 11:39:32 +0200

    :children_crossing: Improve UX in the admin when viewing log records
    * Split the fields into request and response
    * Expose controls to change the request/response encoding
    * Moved decoded req/res body display into main admin fields
    * Ensure there are proper descriptions for the fields

--- log_outgoing_requests/models.py
+++ log_outgoing_requests/models.py
@@ -19,7 +19,6 @@ class OutgoingRequestsLog(models.Model):
     url = models.URLField(
         verbose_name=_("URL"),
         max_length=1000,
-        default="",
         help_text=_("The url of the outgoing request."),
     )
 
@@ -27,7 +26,6 @@ class OutgoingRequestsLog(models.Model):
     hostname = models.CharField(
         verbose_name=_("Hostname"),
         max_length=255,
-        default="",
         help_text=_("The netloc/hostname part of the url."),
     )
     params = models.TextField(
@@ -47,45 +45,53 @@ class OutgoingRequestsLog(models.Model):
         blank=True,
         help_text=_("The type of request method."),
     )
+
+    # Request content
     req_content_type = models.CharField(
         verbose_name=_("Request content type"),
         max_length=50,
-        default="",
         help_text=_("The content type of the request."),
     )
+    req_headers = models.TextField(
+        verbose_name=_("Request headers"), help_text=_("The request headers.")
+    )
+    req_body_encoding = models.CharField(
+        _("Request encoding"),
+        max_length=24,
+        blank=True,
+        help_text=_(
+            "The encoding is either extracted from the Content-Type header or, if "
+            "absent, taken from the default encoding configured for the content type. "
+            "If the decoded request content is not displaying correctly, you may try "
+            "changing the encoding value here."
+        ),
+    )
+    req_body = models.BinaryField(
+        verbose_name=_("Request body"), default=b"", help_text=_("The request body.")
+    )
+
+    # Response content
     res_content_type = models.CharField(
         verbose_name=_("Response content type"),
         max_length=50,
-        default="",
         help_text=_("The content type of the response."),
     )
-    req_headers = models.TextField(
-        verbose_name=_("Request headers"),
-        default="",
-        help_text=_("The request headers."),
-    )
     res_headers = models.TextField(
-        verbose_name=_("Response headers"),
-        default="",
-        help_text=_("The response headers."),
-    )
-    req_body = models.BinaryField(
-        verbose_name=_("Request body"),
-        default=b"",
-        help_text=_("The request body."),
+        verbose_name=_("Response headers"), help_text=_("The response headers.")
     )
     res_body = models.BinaryField(
-        verbose_name=_("Response body"),
-        default=b"",
-        help_text=_("The response body."),
-    )
-    req_body_encoding = models.CharField(
-        max_length=24,
-        default="",
+        verbose_name=_("Response body"), default=b"", help_text=_("The response body.")
     )
     res_body_encoding = models.CharField(
+        _("Response encoding"),
         max_length=24,
-        default="",
+        blank=True,
+        help_text=_(
+            "The encoding is either extracted from the Content-Type header or, if "
+            "absent, taken from the default encoding configured for the content type. "
+            "If the decoded response content is not displaying correctly, you may try "
+            "changing the encoding value here."
+        ),
     )
     response_ms = models.PositiveIntegerField(
         verbose_name=_("Response in ms"),
@@ -98,7 +104,6 @@ class OutgoingRequestsLog(models.Model):
     )
     trace = models.TextField(
         verbose_name=_("Trace"),
-        default="",
         help_text=_("Text providing information in case of request failure."),
     )
 
@@ -143,12 +148,16 @@ class OutgoingRequestsLog(models.Model):
         """
         return self._decode_body(self.req_body, self.req_body_encoding)
 
+    request_body_decoded.short_description = _("Request body")  # type: ignore
+
     @cached_property
     def response_body_decoded(self) -> str:
         """
         Decoded response body for use in template.
         """
         return self._decode_body(self.res_body, self.res_body_encoding)
+
+    response_body_decoded.short_description = _("Response body")  # type: ignore
 
 
 def get_default_max_content_length():
