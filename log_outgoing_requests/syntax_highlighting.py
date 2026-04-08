da88130cc8853c26c14cf0d40f19e4f31b42d94f
Sergei Maertens <sergei@maykinmedia.nl>
2026-04-08 11:48:54 +0200

    :sparkles: [#2] Run formatter before adding syntax highlighting

--- log_outgoing_requests/syntax_highlighting.py
+++ log_outgoing_requests/syntax_highlighting.py
@@ -1,3 +1,7 @@
+import json
+from collections.abc import Callable, Mapping
+
+from django.http.request import MediaType
 from django.utils.safestring import mark_safe
 
 from pygments import highlight
@@ -13,6 +17,28 @@ _FORMATTER = HtmlFormatter(
 )
 
 
+CONTENT_TYPE_TO_FORMATTER: Mapping[str, Callable[[str], str]] = {
+    "json": lambda body: json.dumps(json.loads(body), indent=2),
+    "xml": lambda body: body,
+    "noop": lambda body: body,
+}
+
+
+def format_body(body: str, content_type: str) -> str:
+    """
+    Given the content type, prettify the body content.
+    """
+    assert body, "Can't operate on empty body"
+    assert content_type, "Needs content-type information"
+    if "json" in content_type:
+        formatter = CONTENT_TYPE_TO_FORMATTER["json"]
+    elif "xml" in content_type:
+        formatter = CONTENT_TYPE_TO_FORMATTER["xml"]
+    else:
+        formatter = CONTENT_TYPE_TO_FORMATTER["noop"]
+    return formatter(body)
+
+
 def highlight_body(body: str, content_type: str) -> str:
     """
     Highlight the body, best effort.
@@ -26,6 +52,16 @@ def highlight_body(body: str, content_type: str) -> str:
     """
     if not body:
         return "-"
+
+    # normalize the content type
+    # https://datatracker.ietf.org/doc/html/rfc6838#section-4.2.8 specifies the
+    # structured syntax name suffix, necessary to process content types like soap+xml /
+    # hal+json / problem+json etc...
+    media_type = MediaType(content_type)
+    if "+" in media_type.sub_type:
+        normalized_sub_type = media_type.sub_type.rsplit("+")[1]
+        content_type = f"{media_type.main_type}/{normalized_sub_type}"
+
     if not content_type:
         return body
 
@@ -34,7 +70,8 @@ def highlight_body(body: str, content_type: str) -> str:
     except ClassNotFound:
         return body
 
-    result = highlight(body, lexer, _FORMATTER)
+    formatted_body = format_body(body, content_type)
+    result = highlight(formatted_body, lexer, _FORMATTER)
     return mark_safe(result)
 
 
